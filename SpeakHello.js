fa8443d14e3aa894eb947e7b1ffaff2c272f7955
pavankalyan-47 <63800453+pavankalyan-47@users.noreply.github.com>
2020-05-01 20:35:23 +0530

    Delete SpeakHello.js

--- SpeakHello.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-    var helloSpeaker = new Object();
-    helloSpeaker.speak = function(name) {
-        console.log("Hello  " + name);
-    };
-    window.helloSpeaker = helloSpeaker;
-})(window);
