41ec0a29c4bf9e330c1a9890d234d2efd3be96dc
pavankalyan-47 <63800453+pavankalyan-47@users.noreply.github.com>
2020-05-01 20:35:10 +0530

    Delete SpeakGoodBye.js

--- SpeakGoodBye.js
+++
@@ -1,7 +0,0 @@
-(function(window) {
-    var byeSpeaker = new Object();
-    byeSpeaker.speak = function speak(name) {
-        console.log("Good Bye " + name);
-    };
-    window.byeSpeaker = byeSpeaker;
-})(window);
